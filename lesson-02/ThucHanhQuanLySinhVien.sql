e432153cfe6c31e717f8a2714791334a91fd5c8f
dinh <hieun4587@gmail.com>
2023-12-01 09:28:30 +0700

    lesson3

--- lesson-02/ThucHanhQuanLySinhVien.sql
+++ lesson-02/ThucHanhQuanLySinhVien.sql
@@ -85,3 +85,35 @@ WHERE C.ClassName = 'A1';
 SELECT S.StudentId, S.StudentName, Sub.SubName, M.Mark
 FROM Student S join Mark M on S.StudentId = M.StudentId join Subject Sub on M.SubId = Sub.SubId
 WHERE Sub.SubName = 'CF';
+
+
+-- Hiển thị tất cả các sinh viên có tên bắt đầu bảng ký tự ‘h’
+select * 
+from Student
+where Student.StudentName  like 'h%';
+
+-- Hiển thị các thông tin lớp học có thời gian bắt đầu vào tháng 12.
+select * 
+from Class 
+where Month(StartDate)=12;
+
+-- Hiển thị tất cả các thông tin môn học có credit trong khoảng từ 3-5.
+select * 
+from Subject
+where Credit between 3 and 5;
+
+-- Thay đổi mã lớp(ClassID) của sinh viên có tên ‘Hung’ là 2.
+update Student
+set ClassID = 2 
+where StudentName = 'Hung';
+set sql_safe_updates = 0;
+
+-- Hiển thị các thông tin: StudentName, SubName, Mark. Dữ liệu sắp xếp theo điểm thi (mark) giảm dần. nếu trùng sắp theo tên tăng dần.
+select Student.StudentName,Subject.SubName,Mark
+from Student
+join Subject 
+join Mark
+order by mark desc, Student.StudentName asc;
+
+
+select * from student;
